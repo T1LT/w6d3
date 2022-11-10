3eb31cafe4999b0dcdd8196dfb7c33c1d15e1383
Nishant Racherla <nishant.racherla@gmail.com>
2022-11-09 16:15:06 -0800

    finished phase 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   def index
-    
     render plain: "whatever you want - Nishant"
+  end
+
+  def create
+    render json: params
+  end
+
+  def show
+    render json: params
   end
 end
